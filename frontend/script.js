c6564e5924fd9a0c2ed0529a9d96068ad0768ca6
Leoz168 <leozhu041210@gmail.com>
2024-06-01 13:00:26 -0400

    update frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,15 @@
 async function getWeather() {
     const city = document.getElementById('cityInput').value;
-    console.log(`Fetching weather for city: ${city}`);  // Log city name
+    console.log(`Fetching weather for city: ${city}`);
 
     try {
         const response = await fetch(`http://localhost:5000/weather?city=${city}`);
-        console.log('Response:', response);  // Log the response object
-        
+        console.log('Response:', response);
+
         if (response.ok) {
             const data = await response.json();
-            console.log('Data:', data);  // Log the data
-            
+            console.log('Data:', data);
+
             document.getElementById('temperature').innerText = `Temperature: ${data.temp_c.toFixed(2)}˚C or ${data.temp_f.toFixed(2)}˚F`;
             document.getElementById('feelsLike').innerText = `Feels Like: ${data.feels_c.toFixed(2)}˚C or ${data.feels_f.toFixed(2)}˚F`;
             document.getElementById('humidity').innerText = `Humidity: ${data.humidity}%`;
@@ -18,7 +18,7 @@ async function getWeather() {
             document.getElementById('sunrise').innerText = `Sunrise: ${data.sunrise}`;
             document.getElementById('sunset').innerText = `Sunset: ${data.sunset}`;
 
-            document.getElementById('weatherResult').classList.remove('hidden');
+            document.getElementById('weatherResult').classList.remove('d-none');
         } else {
             console.error('Error fetching weather data:', response.statusText);
         }
